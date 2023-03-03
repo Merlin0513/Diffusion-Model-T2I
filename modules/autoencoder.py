32aa758f110f7e8a51293a0c37e8b6a36681d35d
Wenqing Yang <91286947+Merlin0513@users.noreply.github.com>
2023-03-02 23:20:26 -0500

    Update autoencoder.py

--- modules/autoencoder.py
+++ modules/autoencoder.py
@@ -5,10 +5,10 @@ from contextlib import contextmanager
 
 from taming.modules.vqvae.quantize import VectorQuantizer
 
-from ldm.modules.diffusionmodules.model import Encoder, Decoder
-from ldm.modules.distributions.distributions import DiagonalGaussianDistribution
+from diffusionmodules.model import Encoder, Decoder
+from distributions.distributions import DiagonalGaussianDistribution
 
-from ldm.util import instantiate_from_config
+from util import instantiate_from_config
 
 
 class VQModel(pl.LightningModule):
